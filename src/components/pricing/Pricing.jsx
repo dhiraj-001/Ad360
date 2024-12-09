60c83e7b341bcb3b4c77dc865dfe355d0d930829
dhiraj-001 <gogoidhiraj207@gmail.com>
2024-12-09 17:06:38 +0530

    fourth

--- src/components/pricing/Pricing.jsx
+++ src/components/pricing/Pricing.jsx
@@ -1,54 +1,109 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+import gsap from 'gsap';
+import { useGSAP } from '@gsap/react';
+import { useRef } from 'react';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+
 
 function Pricing() {
+  const [value, setValue] = useState(1); // Default value
+  const[isHid, setIsHid] = useState(false); 
+  const handleChange = (event) => {
+    setValue(event.target.value);
+  };
+
+  useEffect(() => {
+    setIsHid(value == 5); // Set hidden state based on value
+  }, [value]);
+
+
+  
   return (
     <div id="pricing">
-      <img className="budget__coin1" src="https://drbgg29kvmub6.cloudfront.net/assets/ad360/Coin1.svg" alt="gold-coin-img" />
-      <img className="budget__coin2" src="https://drbgg29kvmub6.cloudfront.net/assets/ad360/Coin2.svg" alt="gold-coin-img" />
-      <span className="poppins budget__heading">Pick the right plan for your ad budget</span>
-      <h2 className="poppins budget__subHeading">My monthly ad spend is up to <span>$4500</span></h2>
-      <span className="MuiSlider-root MuiSlider-colorPrimary MuiSlider-sizeMedium budget__slider css-exkjcx">
-        <span className="MuiSlider-rail css-b04pc9"></span>
-        <span className="MuiSlider-track css-1t2bqnt" style={{ left: '0%', width: '20%' }}></span>
-        <span data-index="0" className="MuiSlider-mark MuiSlider-markActive css-17lmo96" style={{ left: '0%' }}></span>
-        <span data-index="1" className="MuiSlider-mark MuiSlider-markActive css-17lmo96" style={{ left: '20%' }}></span>
-        <span data-index="2" className="MuiSlider-mark css-1ejytfe" style={{ left: '40%' }}></span>
-        <span data-index="3" className="MuiSlider-mark css-1ejytfe" style={{ left: '60%' }}></span>
-        <span data-index="4" className="MuiSlider-mark css-1ejytfe" style={{ left: '80%' }}></span>
-        <span data-index="5" className="MuiSlider-mark css-1ejytfe" style={{ left: '100%' }}></span>
-        <span data-index="0" className="MuiSlider-thumb MuiSlider-thumbSizeMedium MuiSlider-thumbColorPrimary css-1s3fy7y" style={{ left: '20%' }}>
-          <input
-            data-index="0"
-            aria-label="Temperature"
-            aria-valuenow="1"
-            aria-valuetext="1°C"
-            aria-orientation="horizontal"
-            aria-valuemax="5"
-            aria-valuemin="0"
-            type="range"
-            min="0"
-            max="5"
-            step="1"
-            value="1"
-            style={{ border: '0px', clip: 'rect(0px, 0px, 0px, 0px)', height: '100%', margin: '-1px', overflow: 'hidden', padding: '0px', position: 'absolute', whiteSpace: 'nowrap', width: '100%', direction: 'ltr' }}
-          />
-        </span>
+      <img
+        className="budget__coin1"
+        src="https://drbgg29kvmub6.cloudfront.net/assets/ad360/Coin1.svg"
+        alt="gold-coin-img"
+      />
+      <img
+        className="budget__coin2"
+        src="https://drbgg29kvmub6.cloudfront.net/assets/ad360/Coin2.svg"
+        alt="gold-coin-img"
+      />
+      <span className="poppins budget__heading">
+        Pick the right plan for your ad budget
       </span>
+      <h2 className="poppins budget__subHeading">
+        My monthly ad spend is up to <span>${value*4500}</span>
+      </h2>
+
+      <div className="slider-container1">
+        <input
+          type="range"
+          min="0"
+          max="5"
+          value={value}
+          onChange={handleChange}
+          className="slider1"
+          step="1"
+        />
+
+        <div className="slider-track"></div>
+      </div>
+
       <div className="budget__plansCards">
-        <div id="plansCard" className="dflexc justifycenter planCard planCardSelected">
+        <div
+          id="plansCard"
+          className={`dflexc justifycenter planCard planCardSelected ${isHid ? 'hidden' : ''}`}    
+          
+          >
           <h3 className="plansCard__heading poppins">Plan</h3>
-          <h2 className="plansCard__price poppins">$199<span>/month</span></h2>
-          <p className="plansCard__overages poppins"><span>Overages</span> apply if ad spend limit is exceeded</p>
-          <a href="https://enterprise.zocket.com/" target="_blank" rel="noopener noreferrer" className="plansCard__button poppins-semiBold dflexr justifycenter planCard__buttonSelected">Proceed with this</a>
+          <h2 className="plansCard__price poppins">
+            $199<span>/month</span>
+          </h2>
+          <p className="plansCard__overages poppins">
+            <span>Overages</span> apply if ad spend limit is exceeded
+          </p>
+          <a
+            href="https://enterprise.zocket.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="plansCard__button poppins-semiBold dflexr justifycenter planCard__buttonSelected"
+          >
+            Proceed with this
+          </a>
           <p className="plansCard__excludingGst inter">*Excluding GST</p>
-          <img className="plansCard__purpleTickCircle" src="https://drbgg29kvmub6.cloudfront.net/assets/ad360/purple-tick-circle.svg" alt="purple-tick-circle" />
+          <img
+            className="plansCard__purpleTickCircle"
+            src="https://drbgg29kvmub6.cloudfront.net/assets/ad360/purple-tick-circle.svg"
+            alt="purple-tick-circle"
+          />
         </div>
-        <div id="plansCard" className="dflexc justifycenter planCard planCardSelected">
+        <div
+          id="plansCard"
+          className="dflexc justifycenter planCard planCardSelected"
+        >
           <h3 className="plansCard__heading poppins">Higher ad spends?</h3>
           <h2 className="plansCard__price poppins">Custom Price</h2>
-          <p className="plansCard__overages poppins"><span><br /></span></p>
-          <a href="https://ad360.ai/book-a-demo/" target="_blank" rel="noopener noreferrer" className="plansCard__button poppins-semiBold dflexr justifycenter planCard__buttonSelected">Talk to us</a>
+          <p className="plansCard__overages poppins">
+            <span>
+              <br />
+            </span>
+          </p>
+          <a
+            href="https://ad360.ai/book-a-demo/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="plansCard__button poppins-semiBold dflexr justifycenter planCard__buttonSelected"
+          >
+            Talk to us
+          </a>
           <p className="plansCard__excludingGst inter">*Excluding GST</p>
+          <img
+            className={`plansCard__purpleTickCircle ${!isHid ? 'hidden' : '' }`}
+            src="https://drbgg29kvmub6.cloudfront.net/assets/ad360/purple-tick-circle.svg"
+            alt="purple-tick-circle"
+          />
         </div>
       </div>
     </div>
--- src/components/product/Product.jsx
+++ src/components/product/Product.jsx
@@ -4,7 +4,20 @@ import img2 from "../images/whyChoose2.webp";
 import img3 from "../images/whyChoose3.webp";
 import img4 from "../images/whyChoose4.webp";
 import img5 from "../images/whyChoose5.png";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+import { useRef } from "react";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 function Product() {
+  // useGSAP(
+  //   () => {
+  //     // gsap code here...
+  //     gsap.from(".boxx1", {
+  //        x: 0
+  //        }); // <-- automatically reverted
+  //   },
+  //   { scope: container }
+  // );
   return (
     <div id="about">
       <h2 className="poppins whyChoose__heading">Why choose Ad360?</h2>
@@ -18,73 +31,98 @@ function Product() {
         ssr=""
         uid="ZT8bQ5"
       ></astro-island>
-     <section className="whyChoose__wrapper">
-  <div className="whyChoose__wrapper__flex" id="whyChoose__wrapper__left">
-    <div className="whyChoose__wrapper1 whyChoose__wrapperImg">
-      <img
-        className="animation__img"
-        src="https://drbgg29kvmub6.cloudfront.net/assets/ad360/whyChoose1.webp"
-        alt="Instant Account Top-Up"
-      />
-    </div>
-    <div className="whyChooseLine">
-      <div></div>
-      <span className="wrapperMiddle__first poppins">1</span>
-    </div>
-  </div>
-  <div className="scroll">
-    <div
-      className="scrollAnimation"
-      data-scroll="in"
-      style={{ "--visible-y": 0.6435, "--viewport-y": -0.7129 }}
-    >
-      <h2 className="poppins whyChoose__cardHeadingAnimation">Instant Account Top-Up</h2>
-      <p className="poppins whyChoose__cardSubHeading">
-        Our automated top-up system is a game-changer compared to the manual, time-consuming process used by our competitors. With Ad360, your account is recharged in seconds, ensuring continuous ad campaign momentum.
-      </p>
-    </div>
-    <div
-      className="scrollAnimation"
-      data-scroll="out"
-      style={{ "--visible-y": 0, "--viewport-y": -1 }}
-    >
-      <h2 className="poppins whyChoose__cardHeadingAnimation">Advanced Analytics Dashboard</h2>
-      <p className="poppins whyChoose__cardSubHeading">
-        While others lack efficient monitoring tools, Ad360 provides a comprehensive analytics dashboard. Gain valuable insights into your Meta and Google ads easily, making data-driven decisions without the complexity.
-      </p>
-    </div>
-    <div
-      className="scrollAnimation"
-      data-scroll="out"
-      style={{ "--visible-y": 0, "--viewport-y": -1 }}
-    >
-      <h2 className="poppins whyChoose__cardHeadingAnimation">Expert Marketing Guidance</h2>
-      <p className="poppins whyChoose__cardSubHeading">
-        Ad360 offers expert consultations to navigate ad policies, a service often absent in competitors’ offerings. We guide you in maintaining policy compliance, ensuring your ads run smoothly without policy infringements.
-      </p>
-    </div>
-    <div
-      className="scrollAnimation"
-      data-scroll="out"
-      style={{ "--visible-y": 0, "--viewport-y": -1 }}
-    >
-      <h2 className="poppins whyChoose__cardHeadingAnimation">Transparent and Economical Pricing</h2>
-      <p className="poppins whyChoose__cardSubHeading">
-        Stand out with our transparent pricing model. Unlike competitors who charge a percentage of ad spend, Ad360's fixed fee structure makes it a more budget-friendly and predictable option for your advertising needs.
-      </p>
-    </div>
-    <div
-      className="scrollAnimation"
-      data-scroll="out"
-      style={{ "--visible-y": 0, "--viewport-y": -1 }}
-    >
-      <h2 className="poppins whyChoose__cardHeadingAnimation">Professional and Recognized Partner</h2>
-      <p className="poppins whyChoose__cardSubHeading">
-        Ad360, a professionally managed startup and a technology partner with Meta, offers more than just a temporary solution, unlike smaller competitors. Our established partnerships and startup vigor ensure cutting-edge, reliable services.
-      </p>
-    </div>
-  </div>
-</section>
+      <section className="whyChoose__wrapper">
+        <div className="whyChoose__wrapper__flex" id="whyChoose__wrapper__left">
+          <div className="whyChoose__wrapper1 whyChoose__wrapperImg">
+            <img
+              className="animation__img"
+              src="https://drbgg29kvmub6.cloudfront.net/assets/ad360/whyChoose1.webp"
+              alt="Instant Account Top-Up"
+            />
+          </div>
+          <div className="whyChooseLine">
+            <div></div>
+            <span className="wrapperMiddle__first poppins">1</span>
+          </div>
+        </div>
+        <div className="scroll">
+          <div
+            className="scrollAnimation boxx1"
+            data-scroll="in"
+            style={{ "--visible-y": 0.6435, "--viewport-y": -0.7129 }}
+          >
+            <h2 className="poppins whyChoose__cardHeadingAnimation">
+              Instant Account Top-Up
+            </h2>
+            <p className="poppins whyChoose__cardSubHeading">
+              Our automated top-up system is a game-changer compared to the
+              manual, time-consuming process used by our competitors. With
+              Ad360, your account is recharged in seconds, ensuring continuous
+              ad campaign momentum.
+            </p>
+          </div>
+          <div
+            className="scrollAnimation"
+            data-scroll="out"
+            style={{ "--visible-y": 0, "--viewport-y": -1 }}
+          >
+            <h2 className="poppins whyChoose__cardHeadingAnimation">
+              Advanced Analytics Dashboard
+            </h2>
+            <p className="poppins whyChoose__cardSubHeading">
+              While others lack efficient monitoring tools, Ad360 provides a
+              comprehensive analytics dashboard. Gain valuable insights into
+              your Meta and Google ads easily, making data-driven decisions
+              without the complexity.
+            </p>
+          </div>
+          <div
+            className="scrollAnimation"
+            data-scroll="out"
+            style={{ "--visible-y": 0, "--viewport-y": -1 }}
+          >
+            <h2 className="poppins whyChoose__cardHeadingAnimation">
+              Expert Marketing Guidance
+            </h2>
+            <p className="poppins whyChoose__cardSubHeading">
+              Ad360 offers expert consultations to navigate ad policies, a
+              service often absent in competitors’ offerings. We guide you in
+              maintaining policy compliance, ensuring your ads run smoothly
+              without policy infringements.
+            </p>
+          </div>
+          <div
+            className="scrollAnimation"
+            data-scroll="out"
+            style={{ "--visible-y": 0, "--viewport-y": -1 }}
+          >
+            <h2 className="poppins whyChoose__cardHeadingAnimation">
+              Transparent and Economical Pricing
+            </h2>
+            <p className="poppins whyChoose__cardSubHeading">
+              Stand out with our transparent pricing model. Unlike competitors
+              who charge a percentage of ad spend, Ad360's fixed fee structure
+              makes it a more budget-friendly and predictable option for your
+              advertising needs.
+            </p>
+          </div>
+          <div
+            className="scrollAnimation"
+            data-scroll="out"
+            style={{ "--visible-y": 0, "--viewport-y": -1 }}
+          >
+            <h2 className="poppins whyChoose__cardHeadingAnimation">
+              Professional and Recognized Partner
+            </h2>
+            <p className="poppins whyChoose__cardSubHeading">
+              Ad360, a professionally managed startup and a technology partner
+              with Meta, offers more than just a temporary solution, unlike
+              smaller competitors. Our established partnerships and startup
+              vigor ensure cutting-edge, reliable services.
+            </p>
+          </div>
+        </div>
+      </section>
       <div className="whyChoose__responsive">
         <div className="dflexr spacebetween whyChoose__responsiveCard whyChoose__wrapper1">
           <img
